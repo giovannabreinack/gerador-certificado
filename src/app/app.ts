4499dd8e9624588715626bba84430c22a3af9268
Gih <giovannabreinackcolombara@gmail.com>
2025-06-26 23:01:11 -0300

    add primary button

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
+import { CommonModule } from '@angular/common';
+import { PrimaryButton } from './_components/primary-button/primary-button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
