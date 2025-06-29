4d31e384ceab547f8b87b22bf0fc4b81e094dd17
Gih <giovannabreinackcolombara@gmail.com>
2025-06-29 20:19:52 -0300

    add componentes de itens da lista de certificados

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
 import { PrimaryButton } from './_components/primary-button/primary-button';
 import { SecondaryButton } from "./_components/secondary-button/secondary-button";
+import { ItemCertificado } from "./_components/item-certificado/item-certificado";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
